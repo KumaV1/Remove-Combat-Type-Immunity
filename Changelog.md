00eede73a931482ca1ba7ff1ed6d45d85cc27cc6
KumaV1 <KumaV1>
2025-11-24 21:23:17 +0100

    Removed test console logs

--- Changelog.md
+++ Changelog.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## v1.0.1
+* Removed test console logs
+
 ## v1.0.0
 * Initial implementation
--- src/setup.ts
+++ src/setup.ts
@@ -1,13 +1,9 @@
 import '../assets/Logo.png'
 
 export async function setup(ctx: Modding.ModContext) {
-    console.log("Test");
-
     ctx.patch(Character, 'isImmuneTo').replace(function (original: (attacker: Character) => boolean, attacker: Character): boolean {
         // Explicitly removed all combat type immunity checks. May become outdated, if existing logic of method were to change
         // NOTE: Had to be done here, as `CharacterModifierTable.getImmunity(attackType)` unfortunately does not include `otherStyleImmunity`
         return this.damageType.immuneTo.has(attacker.damageType);
     });
-
-    console.log("Test2");
 }
